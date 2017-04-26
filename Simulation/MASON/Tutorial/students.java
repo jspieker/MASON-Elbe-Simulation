6a09d5f755fd06c444bf00e37d8a991eacb8ae6c
Alexander <Alexander.eguchi@uni-oldenburg.de>
2017-04-26 07:30:43 +0200

    ReadMe bearbeitet

--- Simulation/MASON/Tutorial/students.java
+++ Simulation/MASON/Tutorial/students.java
@@ -1,10 +1,25 @@
 package Tutorial;
 
-public class students {
+import sim.engine.*;
+
+public class students extends SimState {
 
 	
-	public void main(String args[]){
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4613919932312173669L;
+
+	public students(long seed) {
 		
+		super(seed);
+		
+	}
+
+	public void main(String[] args){
+		doLoop(students.class, args);
+		System.exit(0);
 	}
 	
 }
