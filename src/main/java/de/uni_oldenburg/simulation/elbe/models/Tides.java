5b74cf5fcdf4ca06487e64334b2a9b079d14e813
Hendrik Kahlen <hendrik.kahlen@uni-oldenburg.de>
2017-05-14 17:24:27 +0200

    #6 test: Test added and feature made more accurate.

--- src/main/java/de/uni_oldenburg/simulation/elbe/models/Tides.java
+++ src/main/java/de/uni_oldenburg/simulation/elbe/models/Tides.java
@@ -28,8 +28,11 @@ public class Tides {
 	 * @param highTidePeriod  Is the time needed for the high tide in seconds.
 	 * @param lowTidePeriod   Is the time needed for the low tide in seconds.
 	 * @param isHighTideFirst Determines whether the simulation starts with high tide or not (low tide).
+	 * @throws Exception Is thrown if highTidePeriod or lowTidePeriod are set inappropriate.
 	 */
-	public Tides(final long highTidePeriod, final long lowTidePeriod, final boolean isHighTideFirst) {
+	public Tides(final long highTidePeriod, final long lowTidePeriod, final boolean isHighTideFirst) throws Exception {
+		if (highTidePeriod <= 0) throw new Exception("The high tide period must not be negative or zero.");
+		if (lowTidePeriod <= 0) throw new Exception("The low tide period must not be negative or zero.");
 		this.highTidePeriod = highTidePeriod;
 		this.lowTidePeriod = lowTidePeriod;
 		this.moonAttraction = 0.0;
@@ -39,9 +42,11 @@ public class Tides {
 	/**
 	 * Computes the attraction of the moon with the sinus function: sin((pi/highTide*isHighTide+lowTide*!isHighTide))*x). Afterwards the moon attraction can be got using {@link Tides#getMoonAttraction()} and used as a multiplier to the current water level. The switching between low and high tide is done automatically.
 	 *
-	 * @param time Is the time value at which the moon attraction is wanted.
+	 * @param time Is the time value at which the moon attraction is wanted in seconds.
+	 * @throws Exception Is thrown if the time in seconds is negative.
 	 */
-	public void computeMoonAttraction(long time) {
+	public void computeMoonAttraction(long time) throws Exception {
+		if (time < 0) throw new Exception("The time in seconds must not be negative.");
 		boolean isHighTide = false;
 		// we need to reset the time after one whole cycle from low tide to high tide to low tide or the other way around.
 		time %= (highTidePeriod + lowTidePeriod);
@@ -50,7 +55,13 @@ public class Tides {
 		} else if (isHighTideFirst && time >= highTidePeriod || !isHighTideFirst && time < lowTidePeriod) {
 			isHighTide = false;
 		}
-		this.moonAttraction = Math.sin((Math.PI / (isHighTide ? highTidePeriod : lowTidePeriod)) * time);
+
+		if (isHighTideFirst && !isHighTide) { // modulu the number to the seconds within the new one
+			time -= highTidePeriod; // lowTideIsNotSet
+		} else if (!isHighTideFirst && isHighTide) {
+			time -= lowTidePeriod;
+		}
+		this.moonAttraction = Math.sin((Math.PI / (isHighTide ? highTidePeriod : lowTidePeriod)) * time + (isHighTide ? 0 : Math.PI));
 	}
 
 	/**
@@ -59,9 +70,11 @@ public class Tides {
 	 * @param time       Is the time value at which the moon attraction is wanted.
 	 * @param waterLevel Is the previous water level that should be adjusted using the current the moon attraction and to get the current water level.
 	 * @return The current water level adjusted by the current moon attraction.
+	 * @throws Exception Is thrown if either the time in seconds or the water level is negative.
 	 */
-	public double computeWaterLevel(long time, double waterLevel) {
-		computeMoonAttraction(time);
+	public double computeWaterLevel(long time, double waterLevel) throws Exception {
+		if (waterLevel < 0) throw new Exception("The water level cannot be negative.");
+		computeMoonAttraction(time); // throws an exception if and only if time is negative
 		return waterLevel + waterLevel * this.moonAttraction; // adds or subtracts the current moon attraction
 	}
 
@@ -73,6 +86,5 @@ public class Tides {
 	public double getMoonAttraction() {
 		return moonAttraction;
 	}
-
 
 }
--- src/test/java/de/uni_oldenburg/simulation/elbe/models/TidesTest.java
+++ src/test/java/de/uni_oldenburg/simulation/elbe/models/TidesTest.java
@@ -4,24 +4,115 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Tests {@link Tides}.
  */
 public class TidesTest {
 
 	private Tides instance;
+	private long highTidePeriod = 25000;
+	private long lowTidePeriod = 20000;
 
 	@Before
-	public void setUp() {
-		long highTidePeriod = 20000;
-		long lowTidePeriod = 25000;
-		boolean isHighTide = true;
-		instance = new Tides(highTidePeriod, lowTidePeriod, isHighTide);
+	public void setUp() throws Exception {
+
+		boolean isHighTideFirst = true;
+		instance = new Tides(highTidePeriod, lowTidePeriod, isHighTideFirst);
 	}
 
 	@After
 	public void finish() {
-		// nothing to do...
 	}
+
+	// init...
+	@Test(expected = Exception.class)
+	public void TidesConstructor_passWrongParams_throwsException() throws Exception {
+		try { // catch the first exception to test the second parameter
+			new Tides(-1, 1, true);
+		} catch (Exception e) {
+			new Tides(1, -1, false);
+		}
+	}
+
+	// computeMoonAttraction()
+	@Test(expected = Exception.class)
+	public void computeMoonAttraction_passNegativeTime_throwsException() throws Exception {
+		long time = -1;
+		instance.computeMoonAttraction(time);
+	}
+
+	@Test
+	public void computeMoonAttraction_passCorrectHighTideValue_confirms() throws Exception {
+		double moonAttractionExpected = 0.9510565162951536; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D0+and+x%3D15000
+		instance.computeMoonAttraction(15000);
+		assertEquals("The expected moon attraction must met the computed one.", moonAttractionExpected, instance.getMoonAttraction());
+	}
+
+	@Test
+	public void computeMoonAttraction_passCorrectLowTideValue_confirms() throws Exception {
+		instance = new Tides(highTidePeriod, lowTidePeriod, false);
+		double moonAttractionExpected = -0.7071067811865477; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D1+and+x%3D15000
+		instance.computeMoonAttraction(15000);
+		assertEquals("The expected moon attraction must met the computed one.", moonAttractionExpected, instance.getMoonAttraction());
+	}
+
+	@Test
+	public void computeMoonAttraction_passCorrectLowTideValueThenSwitchToHighTide_confirms() throws Exception {
+		instance = new Tides(highTidePeriod, lowTidePeriod, false);
+		double moonAttractionExpected = -0.7071067811865477; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D1+and+x%3D15000
+		instance.computeMoonAttraction(15000);
+		assertEquals("The expected moon attraction must met the computed one.", moonAttractionExpected, instance.getMoonAttraction());
+
+		// switch tide
+		moonAttractionExpected = 0.587785252292473;// see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D0+and+x%3D25000-20000
+		instance.computeMoonAttraction(25000);
+		assertEquals("The expected moon attraction must met the computed one.", moonAttractionExpected, instance.getMoonAttraction());
+	}
+
+	@Test
+	public void computeMoonAttraction_passCorrectHighTideValueThenSwitchToLowTide_confirms() throws Exception {
+		double moonAttractionExpected = 0.9510565162951536; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D0+and+x%3D15000
+		instance.computeMoonAttraction(15000);
+		assertEquals("The expected moon attraction must met the computed one.", moonAttractionExpected, instance.getMoonAttraction());
+
+		// switch tide
+		moonAttractionExpected = -1.0;// see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D1+and+x%3D35000-25000
+		instance.computeMoonAttraction(35000);
+		assertEquals("The expected moon attraction must met the computed one.", moonAttractionExpected, instance.getMoonAttraction());
+
+	}
+
+	// computeWaterLevel():
+	@Test(expected = Exception.class)
+	public void computeWaterLevel_passNegativeTime_throwsException() throws Exception {
+		long time = -1;
+		double waterLevel = 1.0;
+		instance.computeWaterLevel(time, waterLevel);
+	}
+
+	@Test(expected = Exception.class)
+	public void computeWaterLevel_passNegativeWaterLevel_throwsException() throws Exception {
+		long time = 1;
+		double waterLevel = -1.0;
+		instance.computeWaterLevel(time, waterLevel);
+	}
+
+	@Test
+	public void computeWaterLevel_passCorrectValue_confirms() throws Exception {
+		long time = 15000;
+		double waterLevel = 4.0; // 4.0 meters
+
+		double moonAttractionExpected = 0.9510565162951536; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D0+and+x%3D15000
+		double newWaterLevelExpected = waterLevel + waterLevel * moonAttractionExpected;
+
+		double newWaterLevelTest = instance.computeWaterLevel(time, waterLevel);
+
+		assertEquals("The expected water level must met the computed one.", newWaterLevelExpected, newWaterLevelTest);
+	}
+
+
+	// getMoonAttraction(): (implicitly tested)
 
 }
