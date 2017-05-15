0ae190ea47aa597813cf5bc19c3f237662b60c1a
Adrian <adrian.jagusch@we-mail.de>
2017-05-16 01:40:25 +0200

    Added full test coverage

--- src/test/java/de/uni_oldenburg/simulation/elbe/AppStarterTest.java
+++ src/test/java/de/uni_oldenburg/simulation/elbe/AppStarterTest.java
@@ -1,5 +1,6 @@
 package de.uni_oldenburg.simulation.elbe;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by adrian-jagusch on 16.05.17.
  */
 class AppStarterTest {
+
+	private AppStarter appStarter;
+
+	@Before
+	public void setup() {
+		appStarter = new AppStarter();
+	}
 
 	@Test
 	void main() {
