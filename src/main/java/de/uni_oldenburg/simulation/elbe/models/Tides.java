016cff3432e3a7e8a5e48af764b708d341d18d32
Hendrik Kahlen <hendrik.kahlen@uni-oldenburg.de>
2017-05-29 22:32:33 +0200

    #6 Tides compution improved but not working yet.

--- src/main/java/de/uni_oldenburg/simulation/elbe/models/Tides.java
+++ src/main/java/de/uni_oldenburg/simulation/elbe/models/Tides.java
@@ -1,7 +1,5 @@
 package de.uni_oldenburg.simulation.elbe.models;
 
-import java.util.ArrayList;
-
 /**
  * Computes the models of the elbe by using an adapted sinus function: f(t,y) = sin((pi/highTidePeriod)*t) TODO extend
  */
@@ -26,9 +24,7 @@ public class Tides {
 	/**
 	 * Is the length of the Elbe. The parameter is used to determine whether a specific x coordinate is affected by the moon attraction or not.
 	 */
-	private int elbeLength;
-
-	private WaterLevel waterLevel[];
+	private long elbeLength;
 
 	/**
 	 * Tide constructor to initialize the tides context given the parameters.
@@ -38,17 +34,12 @@ public class Tides {
 	 * @param isHighTideFirst Determines whether the simulation starts with high tide or not (low tide).
 	 * @param elbeLength      Is the length of the Elbe. The parameter is used to determine whether a specific x coordinate is affected by the moon attraction or not.
 	 */
-	public Tides(final long highTidePeriod, final long lowTidePeriod, final boolean isHighTideFirst, int elbeLength) {
+	public Tides(final long highTidePeriod, final long lowTidePeriod, final boolean isHighTideFirst, long elbeLength) {
 		this.highTidePeriod = highTidePeriod;
 		this.lowTidePeriod = lowTidePeriod;
 		this.moonAttraction = 0.0;
 		this.isHighTideFirst = isHighTideFirst;
 		this.elbeLength = elbeLength;
-		waterLevel = new WaterLevel[elbeLength];
-
-		for (int i = 0; i < elbeLength; i++) {
-			waterLevel[i] = new WaterLevel(0, i);
-		}
 	}
 
 	/**
@@ -80,13 +71,14 @@ public class Tides {
 	 * @param xCoordinate       Is a x coordinate at the length of the elbe. The parameter is used to determine whether the position is affected by the moon attraction or not.
 	 * @return The current water level adjusted by the current moon attraction or -1 if the x coordinate is not affected yet.
 	 */
-	public double computeWaterLevel(long time, double averageWaterLevel, int xCoordinate) {
+	public double computeWaterLevel(long time, double averageWaterLevel, long xCoordinate) {
 
 		computeMoonAttraction(time);
 		if (isAffected(time, xCoordinate)) {
-			waterLevel[xCoordinate].updateWaterLevel(averageWaterLevel + averageWaterLevel * this.moonAttraction); // adds or subtracts the current moon attraction
+			return averageWaterLevel + averageWaterLevel * this.moonAttraction; // adds or subtracts the current moon attraction
+		} else {
+			return -1;
 		}
-		return waterLevel[xCoordinate].getLastKnownWaterLevel();
 	}
 
 	/**
@@ -99,7 +91,7 @@ public class Tides {
 	}
 
 	private boolean isAffected(long time, long xCoordinate) {
-
+		// TODO rework!!!!
 		// we need to reset the time after one whole cycle from low tide to high tide to low tide or from high tide to low tide to high tide.
 		time %= (highTidePeriod + lowTidePeriod);
 		boolean isHighTide = isHighTide(time);
@@ -110,10 +102,26 @@ public class Tides {
 			time -= lowTidePeriod;
 		}
 
-		if (isHighTide && time >= ((double) highTidePeriod) / 2 || !isHighTide && time >= ((double) lowTidePeriod) / 2) {
-			return true;
-		} else {
-			double xPositionsAffectedAtTime = ((double) elbeLength / (isHighTide ? ((double) highTidePeriod) / 2 : ((double) lowTidePeriod))) * time; // the time is divided by two because at the top of the sinus function e.g. Hamburg is affected already
+		if (isHighTide && time >= ((double) highTidePeriod) / 2) { // backwards
+			double xPositionsAffectedAtTime = elbeLength * 2 - ((double) elbeLength / ((double) (highTidePeriod / 2))) * time; // the time is divided by two because at the top of the sinus function e.g. Hamburg is affected already
+
+			if (xPositionsAffectedAtTime <= xCoordinate) {
+				System.out.println(xCoordinate + " true");
+				return true;
+			} else {
+				return false;
+			}
+		} else if (!isHighTide && time >= ((double) lowTidePeriod) / 2) { // backwards
+			double xPositionsAffectedAtTime = elbeLength * 2 - ((double) elbeLength / ((double) (lowTidePeriod / 2))) * time; // the time is divided by two because at the top of the sinus function e.g. Hamburg is affected already
+
+			if (xPositionsAffectedAtTime > xCoordinate) {
+				System.out.println(xCoordinate + " true");
+				return true;
+			} else {
+				return false;
+			}
+		} else { // forwards
+			double xPositionsAffectedAtTime = ((double) elbeLength / (isHighTide ? ((double) highTidePeriod) / 2 : ((double) lowTidePeriod) / 2)) * time; // the time is divided by two because at the top of the sinus function e.g. Hamburg is affected already
 
 			if (isHighTide && xPositionsAffectedAtTime >= xCoordinate) {
 				return true;
