22a06f625d94ed5beac1ad27cd7203d528c616d1
Hendrik Kahlen <hendrik.kahlen@uni-oldenburg.de>
2017-06-07 17:05:15 +0200

    #6 Tests extended. Workaround deleted. LevelOfAffection set from  to 0.7.

---
+++ src/test/java/de/uni_oldenburg/simulation/elbe/models/DynamicWaterLevelTest.java
@@ -0,0 +1,52 @@
+package de.uni_oldenburg.simulation.elbe.models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link DynamicWaterLevel}.
+ */
+public class DynamicWaterLevelTest {
+
+	private DynamicWaterLevel instance;
+	private long highTidePeriod = 25000;
+	private long lowTidePeriod = 20000;
+	private int elbeLength = 507 + 230 + 230 + 200 + 48;
+
+	@Before
+	public void setUp() throws Exception {
+		instance = new DynamicWaterLevel(elbeLength, highTidePeriod, lowTidePeriod, true);
+	}
+
+	@After
+	public void finish() {
+		// nothing to do ...
+	}
+
+	@Test
+	public void getCurrentWaterLevels_HighTidePassCorrectValues_confirms() throws Exception {
+
+		long time = 0;
+
+		double[] waterLevels = instance.getCurrentWaterLevels(time);
+
+		for (double waterLevel : waterLevels) {
+			assertEquals("The actual water level must meet the expected one.", Tides.AVERAGE_LOW_TIDE_WATERLEVEL_ABOVE_CD, waterLevel, 0.01);
+		}
+	}
+
+	@Test
+	public void getCurrentWaterLevels_LowTidePassCorrectValues_confirms() throws Exception {
+
+		instance = new DynamicWaterLevel(elbeLength, highTidePeriod, lowTidePeriod, false);
+		long time = 0;
+		double[] waterLevels = instance.getCurrentWaterLevels(time);
+
+		for (double waterLevel : waterLevels) {
+			assertEquals("The actual water level must meet the expected one.", Tides.AVERAGE_HIGH_TIDE_WATERLEVEL_ABOVE_CD, waterLevel, 0.01);
+		}
+	}
+}
--- src/test/java/de/uni_oldenburg/simulation/elbe/models/TidesTest.java
+++ src/test/java/de/uni_oldenburg/simulation/elbe/models/TidesTest.java
@@ -27,11 +27,11 @@ public class TidesTest {
 
 	@Test
 	public void computeWaterLevel_passCorrectValueWithHighTide_confirms() throws Exception {
-		long time = 10000;
-		double waterLevel = 4.9;
+		long time = 25000;
+		double waterLevel = Tides.AVERAGE_HIGH_TIDE_WATERLEVEL_ABOVE_CD;
 
-		double moonAttractionExpected = 0.9510565162951536; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D0+and+x%3D15000
-		double newWaterLevelExpected = waterLevel + waterLevel * moonAttractionExpected;
+		double moonAttractionExpected = 1.0;
+		double newWaterLevelExpected = waterLevel * moonAttractionExpected;
 
 		double newWaterLevelTest = instance.computeWaterLevel(time, 100);
 
@@ -41,75 +41,25 @@ public class TidesTest {
 	@Test
 	public void computeWaterLevel_passCorrectValueWithLowTide_confirms() throws Exception {
 		instance = new Tides(highTidePeriod, lowTidePeriod, false, elbeLength);
-		double moonAttractionExpected = -0.7071067811865475; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D1+and+x%3D15000
-		double waterLevel = 4.9;
 
-		double newWaterLevelExpected = waterLevel + waterLevel * moonAttractionExpected;
-		double newWaterLevelTest = instance.computeWaterLevel(5000, elbeLength - 50);
+		long time = 20000;
+		double newWaterLevelExpected = Tides.AVERAGE_LOW_TIDE_WATERLEVEL_ABOVE_CD;
+
+		double newWaterLevelTest = instance.computeWaterLevel(time, elbeLength - 50);
 
 		assertEquals("The expected water level must met the computed one.", newWaterLevelExpected, newWaterLevelTest);
 	}
 
 	@Test
-	public void computeWaterLevel_passCorrectValueButNotYetAffectedWitHighTide_confirms() throws Exception {
-		long time = 5000;
-		double waterLevel = 4.9;
-
-		double newWaterLevelExpected = -1;
-
-		double newWaterLevelTest = instance.computeWaterLevel(time, elbeLength /* Hamburg */);
-
-		assertEquals("The expected water level must be -1.", newWaterLevelExpected, newWaterLevelTest);
-
-	}
-
-	@Test
-	public void computeWaterLevel_passCorrectValueButNotYetAffectedWitLowTide_confirms() throws Exception {
+	public void others_passCorrectValueWithLowTide_confirms() throws Exception {
 		instance = new Tides(highTidePeriod, lowTidePeriod, false, elbeLength);
 
-		long time = 5000;
-		double waterLevel = 4.9;
+		assertEquals("The actual moon attraction must meet the expected one.", 0.0, instance.getMoonAttraction());
 
-		double newWaterLevelExpected = -1;
+		long time = 15000;
+		double newWaterLevelExpected = Tides.AVERAGE_LOW_TIDE_WATERLEVEL_ABOVE_CD;
 
-		double newWaterLevelTest = instance.computeWaterLevel(time, 0 /* Elbe entry */);
+		double newWaterLevelTest = instance.computeWaterLevel(time, elbeLength - 50);
 
-		assertEquals("The expected water level must be -1.", newWaterLevelExpected, newWaterLevelTest);
-
-	}
-
-	@Test
-	public void computeWaterLevel_passCorrectLowTideValueThenSwitchToHighTide_confirms() throws Exception {
-		instance = new Tides(highTidePeriod, lowTidePeriod, false, elbeLength);
-		double moonAttractionExpected = -0.7071067811865477; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D1+and+x%3D15000
-		double waterLevel = 4.9;
-
-		double newWaterLevelExpected = waterLevel + waterLevel * moonAttractionExpected;
-		double newWaterLevelTest = instance.computeWaterLevel(15000, 0);
-
-		assertEquals("The expected water level must met the computed one.", newWaterLevelExpected, newWaterLevelTest);
-
-		// switch tide
-		moonAttractionExpected = 0.587785252292473;// see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D0+and+x%3D25000-20000
-		newWaterLevelExpected = waterLevel + waterLevel * moonAttractionExpected;
-		newWaterLevelTest = instance.computeWaterLevel(25000, 0);
-		assertEquals("The expected water level must met the computed one.", newWaterLevelExpected, newWaterLevelTest);
-	}
-
-	@Test
-	public void computeWaterLevel_passCorrectHighTideValueThenSwitchToLowTide_confirms() throws Exception {
-		double waterLevel = 4.9;
-
-		double moonAttractionExpected = 0.9510565162951536; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D0+and+x%3D15000
-		double newWaterLevelExpected = waterLevel + waterLevel * moonAttractionExpected;
-		double newWaterLevelTest = instance.computeWaterLevel(10000, 500);
-
-		assertEquals("The expected water level must met the computed one.", newWaterLevelExpected, newWaterLevelTest);
-
-		// switch tide
-		moonAttractionExpected = -0.7071067811865475; // see wolfram alpha at https://www.wolframalpha.com/input/?i=sin((pi%2F(25000-y5000))*x+%2B+(pi*y)),+where+y%3D1+and+x%3D15000
-		newWaterLevelExpected = waterLevel + waterLevel * moonAttractionExpected;
-		newWaterLevelTest = instance.computeWaterLevel(30000, 500);
-		assertEquals("The expected water level must met the computed one.", newWaterLevelExpected, newWaterLevelTest);
 	}
 }
